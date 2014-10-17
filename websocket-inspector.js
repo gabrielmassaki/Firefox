b2301e7a933a5bec5e9e36e8f076134b9157e20a
kibe <kibe@utopia.utopia>
2014-10-17 12:09:13 -0300

    some changes

--- websocket-inspector.js
+++ websocket-inspector.js
@@ -1,18 +1,19 @@
-Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 const protocol = require("devtools/server/protocol");
-const {method, Arg, RetVal} = protocol;
 
 let WebSocketInspector = exports.WebSocketInspector = protocol.ActorClass({
     typeName: "websocketinspector",
-
-
-});
-							       
-WebSocketInspector.prototype = {
     classDescription: "Inspects the WebSockets",
     classID: Components.ID("{CD00FB51-8399-4A59-A823-219568811EB3}"),
     contractID: "@mozilla.org/web-socket-inspector;1",
+    initialize: function(conn, tabActor) {
+	protocol.Actor.prototype.initialize.call(this, conn);
+	this.docshell = tabActor.docShell;
+    },
     onOpen: function() {
 	/*
 	msg = { "from": wsactor, 
@@ -43,4 +44,4 @@ WebSocketInspector.prototype = {
     onMessage: function(data, messageType) {
 	
     }
-}
+});
